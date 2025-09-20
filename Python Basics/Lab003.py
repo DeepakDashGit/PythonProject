d3e192d95da6aac3f04549bf7bc4256b755298a9
DeepakDashGit <ranjandeepak930@gmail.com>
2025-09-20 12:07:29 +0530

    Git reposetry fix

---
+++ Python Basics/Lab003.py
@@ -0,0 +1,10 @@
+day = 20
+#variable_Name = variable_Value
+#Identifier > Assignment operator > Variable Value
+
+print(type(day))
+
+complex_number = 2 + 3j
+print(complex_number.real)
+print(complex_number.imag)
+
