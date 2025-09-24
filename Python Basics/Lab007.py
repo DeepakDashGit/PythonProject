c3a3a64d44d9bc1ee299cc454dce3cb5d70c3c85
DeepakDashGit <ranjandeepak930@gmail.com>
2025-09-24 19:42:51 +0530

    Use of format

--- Python Basics/Lab007.py
+++ Python Basics/Lab007.py
@@ -7,3 +7,7 @@ radius = float(input("Enter Radius:"))
 area = 3.14986784 * pow(radius, 2)
 print("Area of the circle is", area)
 print(f"Area of the circle is: {area:.2f}") #in formating we can use other functions
+
+import math
+area = math.pi * math.pow(radius,2)
+print(f"Area of the circle is: {area:.3f}")
