a5a971ac3ec004ead4ea37772e3d3f83e7237be8
DeepakDashGit <ranjandeepak930@gmail.com>
2025-09-24 19:36:09 +0530

    Use of format

---
+++ Python Basics/Lab006.py
@@ -0,0 +1,12 @@
+a = 5
+b = 5
+c = 5
+a += 1
+b -= 1
+c *= 1
+print(a, b, c)
+
+Age = int(input("Enter Age:"))
+print("You are eligible" if Age >= 18 else "Come later")
+
+
---
+++ Python Basics/Lab007.py
@@ -0,0 +1,9 @@
+radius = float(input("Enter Radius:"))
+area = 3.14 * pow(radius, 2)
+print("Area of the circle is", area)
+print(f"Area of the circle is: {area:.2f}")
+
+radius = float(input("Enter Radius:"))
+area = 3.14986784 * pow(radius, 2)
+print("Area of the circle is", area)
+print(f"Area of the circle is: {area:.2f}") #in formating we can use other functions
