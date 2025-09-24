d0eff333a3ab97a76c6ac2eb7e9008a59dd88c4a
DeepakDashGit <ranjandeepak930@gmail.com>
2025-09-24 19:59:29 +0530

    Use of format

--- Python Basics/Lab007.py
+++ Python Basics/Lab007.py
@@ -11,3 +11,5 @@ print(f"Area of the circle is: {area:.2f}") #in formating we can use other funct
 import math
 area = math.pi * math.pow(radius,2)
 print(f"Area of the circle is: {area:.3f}")
+
+print(3.14 * (float(input("Enter Radius "))**2))
