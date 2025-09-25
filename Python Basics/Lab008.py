4ccfe7e0a6e0eabd1e2b974988573d9fb00fe4ff
DeepakDashGit <ranjandeepak930@gmail.com>
2025-09-25 11:40:51 +0530

    Use of format

--- Python Basics/Lab008.py
+++ Python Basics/Lab008.py
@@ -1,7 +1,15 @@
-a = 10
+a = 10 #input("Enter a number: ")
 print(type(a))
 
 if a == 10:
     print("Hello World")
 else:
     print("Go Away")
+
+b = 12
+c = 13
+print(max(12,13))
+if b > c:
+     print(b)
+else:
+     print(f"{c} is greater")
