7549c922a5c2ca88eefd3cd554b279b7f7f9343b
DeepakDashGit <ranjandeepak930@gmail.com>
2025-09-26 18:05:26 +0530

    loop

--- Python Basics/Lab010.py
+++ Python Basics/Lab010.py
@@ -1,9 +1,3 @@
-# Leap_Year = int(input("Enter Year: "))
-# if (Leap_Year % 400 == 0) or(Leap_Year % 4 == 0 and Leap_Year % 100 != 0) :
-#     print(f"{Leap_Year} is a leap year")
-# else:
-#     print(f"{Leap_Year} is not a leap year")
-
 # for i in range(1,10):
 #     if i == 5:
 #         print("five")
