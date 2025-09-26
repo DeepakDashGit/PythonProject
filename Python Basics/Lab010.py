ba11700728d14b9f42bf87891a509c048b38c12c
DeepakDashGit <ranjandeepak930@gmail.com>
2025-09-26 17:03:02 +0530

    3 table format loop

--- Python Basics/Lab010.py
+++ Python Basics/Lab010.py
@@ -4,8 +4,31 @@
 # else:
 #     print(f"{Leap_Year} is not a leap year")
 
-for i in range(1,10):
-    if i == 5:
-        print("five")
-    else:
-        print(i)
+# for i in range(1,10):
+#     if i == 5:
+#         print("five")
+#     else:
+#         print(i)
+
+# for i in range(1,10):
+#     print(i)
+#     if i == 5:
+#         break
+
+# for i in range(1, 10):
+# 	if i == 6:
+# 		print(i)
+# 	else:
+# 		print("No Output")
+
+# for i in range(1, 10):
+# 	if i == 6 or i == 5:
+# 		print(i)
+# 	else:
+# 		pass
+
+for i in range(10):
+	if i%2 == 0:
+		continue
+	else:
+		print(i)
