b498c45a1a0ce3fe08cc5ac9ddc9f5b9b7134193
DeepakDashGit <ranjandeepak930@gmail.com>
2025-10-11 18:08:19 +0530

    loop

--- Python Basics/Lab013_Loop.py
+++ Python Basics/Lab013_Loop.py
@@ -3,6 +3,24 @@
 #     print(N)
 #     N += 1
 
-n = 5
-for i in range(1, n + 1):
-    print(f"*" * i)
+# n = 5
+# for i in range(1, n + 1):
+#     print(" *" * i)
+
+# n = 5
+# for i in range(1,n):
+#     s = " " * (n-i)
+#     st = "* " * i
+#     print(s + st)
+
+# n = 4
+# for i in range(1,n):
+#     s = " " * (n-i)
+#     st = "*" * (i * 2 - 1)
+#     print(s + st)
+
+n = 4
+for i in range(0,n):
+    s = " " * (n-(i+1))
+    st = "*" * ((i * 2) + 1)
+    print(s + st)
