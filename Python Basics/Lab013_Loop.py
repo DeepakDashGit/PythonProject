aa09d9f9b3205cc6d567cdbb984d2ef4e04c2afe
DeepakDashGit <ranjandeepak930@gmail.com>
2025-10-11 19:50:42 +0530

    loop

--- Python Basics/Lab013_Loop.py
+++ Python Basics/Lab013_Loop.py
@@ -19,8 +19,17 @@
 #     st = "*" * (i * 2 - 1)
 #     print(s + st)
 
+# n = 4
+# for i in range(0,n):
+#     s = " " * (n-(i+1))
+#     st = "*" * ((i * 2) + 1)
+#     print(s + st)
+
 n = 4
-for i in range(0,n):
-    s = " " * (n-(i+1))
-    st = "*" * ((i * 2) + 1)
-    print(s + st)
+for i in range(1,n):
+    space  = " " * (n-(i+1))
+    print(space, end=" ")
+    for j in range(1,i+1):
+        print(j, end=" ")
+    print()
+
