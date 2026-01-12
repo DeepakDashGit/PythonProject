7799baf94f8b05a6af61a7636172b0f06e4f52c8
DeepakDashGit <ranjandeepak930@gmail.com>
2026-01-12 17:22:59 +0530

    14 Oct

---
+++ Task/Task002.py
@@ -0,0 +1,11 @@
+# take user input and print quotient and reminder
+
+num1 = float(input("Enter First Number- "))
+num2 = float(input("Enter Second Number- "))
+
+quotient = num1 // num2
+reminder = num1 % num2
+
+print(quotient)
+print(reminder)
+
