ea6d29a2150d63d60eb367e22def83874193cadc
DeepakDashGit <ranjandeepak930@gmail.com>
2026-02-18 16:49:17 +0530

    28 Oct

---
+++ Task/Task009_Factorial.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number to now the factorial: "))
+
+fact = 1
+
+if num < 0:
+    print("Factorial is not defined")
+elif num == 0:
+    print("Factorial = ", fact)
+else:
+    for i in range(1, num+1):
+        fact *= i
+print("Factorial is", fact)
+
