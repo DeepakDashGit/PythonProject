7934c4eab516ee6b51d9e64957d4e885f1311ba3
DeepakDashGit <ranjandeepak930@gmail.com>
2026-08-23 19:57:28 +0530

    18 Oct

--- src/ex_05_Conditions & Loop/Lab033_If Condition.py
+++ src/ex_05_Conditions & Loop/Lab033_If Condition.py
@@ -7,24 +7,26 @@ else:
 """
 
 while True:
-    age_input = input("Enter age: ").strip()
+    user_input = input("Enter age: ").strip()
 
-    if age_input == "":
+    if user_input == "":
         print("Input cannot be empty.")
         continue
 
     try:
-        age = float(age_input)
-
-        if age < 0:
-            print("Age cannot be negative.")
-        elif age > 120:
-            print("Invalid age. Enter a realistic value.")
-        elif age >= 21:
-            print("Yes, you are allowed.")
-        else:
-            print("No, you are not allowed.")
-        break
+        age = int(user_input)
 
     except ValueError:
         print("Invalid input. Only numeric values are allowed.")
+        continue
+
+    if age < 0:
+        print("Age cannot be negative.")
+    elif age > 120:
+        print("Invalid age. Enter a realistic value.")
+    elif age >= 21:
+        print("Yes, you are allowed.")
+        break
+    else:
+        print("No, you are not allowed.")
+        break
--- src/ex_07_Loops/While/Lab047_While.py
+++ src/ex_07_Loops/While/Lab047_While.py
@@ -9,3 +9,14 @@ while count < 5:
     count += 1
     
 
+N = 10    # It will give the result of sum of 10 to 1 (10+9+8+7+6+5+4+3+2+1)
+s = 0
+
+while True:
+    s += N
+    N -= 1
+
+    if N == 0:
+        break
+
+print(s)
